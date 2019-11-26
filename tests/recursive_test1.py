feeddc252a047da737240026855e800dc843ff48
Meital Issers <meitalge@gmail.com>
2019-11-26 22:54:52 +0200

    changes

--- tests/recursive_test1.py
+++ tests/recursive_test1.py
@@ -4,60 +4,60 @@
 
 # #7 6 4 2 1 1 /8
 
-numbers = [7, 6, 4, 2, 1, 1]
-S = 8
-answers = []
+# numbers = [7, 6, 4, 2, 1, 1]
+# S = 8
+# answers = []
+#
+# def func1(numbers,target,num_slice = []):
+#     s = sum(num_slice)
+#     print("*** s", s,"num_slice",num_slice,"numbers",numbers)
+#
+#     if s > target:
+#         return
+#     if s == target:
+#         print("sol",num_slice)
+#
+#     for i in range(len(numbers)):
+#         n = numbers[i]
+#         print("i",i,"n", n)
+#         func1(numbers[i+1:],target,num_slice + [n])
+#
+#
+#
+# func1( [7, 4, 1, 2, 6, 1],S)
 
-def check(left,current):
-    if (left + current) > S:
-        return 0
-    return left + current
-
-def sum_by_num(n,num_slice):
-
-    ans = check(n,num_slice[0])
-    if (ans == S):
-        print("found",n,num_slice[0])
+def subset_sum(numbers, target, partial=[], partial_sum=0):
+    if partial_sum == target:
+        yield partial
+    if partial_sum >= target:
         return
-    else:
-        ans = check(n,num_slice[0])
+    for i, n in enumerate(numbers):
+        remaining = numbers[i + 1:]
+        yield from subset_sum(remaining, target, partial + [n], partial_sum + n)
 
+for i in subset_sum([7, 4, 1, 2, 6, 1],8):
+    print(i)
+############################################
 
-def all_sum():
-    for n in range(len(numbers)):
-        print("[all sum] n",n)
-        if (n + 1) == len(numbers):
-            break
-        print("[all sum] numbers[n]",numbers[n],"numbers[n+1]",numbers[n+1])
-        ans = check(numbers[n],numbers[n+1])
-        print("[all sum] ans",ans)
-        if ans == S:
-            answers.append((numbers[n],numbers[n+1]))
-        else:
-            ans = check(numbers[n],numbers[n+2])
+############################################
 
-    print (answers)
-all_sum()
-
-
-
-def subset_sum(numbers, target, partial=[]):
-    s = sum(partial)
-
-    # check if the partial sum is equals to target
-    if s == target:
-        print (partial, target)
-    if s >= target:
-        return  # if we reach the number why bother to continue
-
-    for i in range(len(numbers)):
-        n = numbers[i]
-        remaining = numbers[i+1:]
-        subset_sum(remaining, target, partial + [n])
-
-
-
-subset_sum([3,9,8,4,5,7,10],15)
+# def subset_sum(numbers, target, partial=[]):
+#     s = sum(partial)
+#
+#     # check if the partial sum is equals to target
+#     if s == target:
+#         print (partial, target)
+#     if s >= target:
+#         return  # if we reach the number why bother to continue
+#
+#     for i in range(len(numbers)):
+#         n = numbers[i]
+#         remaining = numbers[i+1:]
+#         subset_sum(remaining, target, partial + [n])
+#
+#
+#
+# subset_sum([3,9,8,4,5,7,10],15)
 
 
      #Outputs:
