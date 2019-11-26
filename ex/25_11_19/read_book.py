92c86105083faae9492b312f603b8367043c1f49
Meital <meitalge@gmail.com>
2019-11-26 11:25:18 +0200

    sample

--- ex/25_11_19/read_book.py
+++ ex/25_11_19/read_book.py
@@ -1,35 +1,57 @@
-def read_book(file_path):
+import re
+
+def clear():
+    str = "1234 56790ab cdefA BCDE?F!@#$%^&*()_+<>?,./"
+    result = re.sub(r'[^a-zA-Z0-9 ]', "", str)
+    print (result)
+
+def read_from_file(file_path):
     file = open(file_path,"r+",encoding="utf-8")
+    file_content = file.read()
+    file.close()
 
-    content = file.read()
-    content_dic = {}
-    print(content)
-    new_content  = []
+    return file_content
 
-    #clean content
+
+def clear_unused_chars(content):
+    clean_content = []
+
     for char in content:
         if char.isalpha() or char.isspace():
-            new_content.append(char)
+            clean_content.append(char)
+        else:
+            clean_content.append(" ")
+    return  clean_content
 
-    # build dictionary to count words
-    print(''.join(new_content),end="******\n")
-    splitted = ''.join(new_content).split()
-    for word in splitted:
-        print(word)
+
+def count_words(file_path):
+
+    content = read_from_file(file_path)
+
+    clean_content = clear_unused_chars(content)
+
+    content_dic = {}
+    (max_word_rep, max_repetition) = (0,0)
+
+    splitted_clean_content = ''.join(clean_content).split()
+    print(splitted_clean_content)
+    for word in splitted_clean_content:
         if not word.isspace():
             if content_dic.get(word.lower()):
                 content_dic[word.lower()] += 1
             else:
                 content_dic[word.lower()] = 1
 
-    #file.write("".join(str(new_content)))
-    #file.flush()
-    file.close()
+            (max_word_rep, max_repetition) = (word.lower(), content_dic[word.lower()]) if max_repetition < content_dic[word.lower()] else (max_word_rep, max_repetition)
 
     print(content_dic)
+    return (max_word_rep, max_repetition)
 
 
-if (__name__== '__main__'):
-    file_path = r'C:\meital\ExperisDSCourse\ex\25_11_19\Shakespeare.txt'
-    read_book(file_path)
+if __name__== '__main__':
+    file_path = r'C:\meital\ExperisDSCourse\ex\25_11_19\test_file.txt'
+    max_word_rep, max_repetition =  count_words(file_path)
+    print(max_word_rep,max_repetition)
+
+    clear()
 
