f86ee910a50d1f8f8689e4d72ab301cfcbab8aac
Meital Issers <meitalge@gmail.com>
2019-11-30 14:00:16 +0200

    changes

--- samples/test2.py
+++ samples/test2.py
@@ -43,19 +43,3 @@ def sum_mul(*args,initial_sum = 0,initial_product = 1,**keywd):
 # new_a = [i for i in a if i %2 == 0]
 # print(new_a)
 
-def fibonacci():
-    num = int(input("How many numbers that generates?:"))
-    i = 1
-    if num == 0:
-        fib = []
-    elif num == 1:
-        fib = [1]
-    elif num == 2:
-        fib = [1,1]
-    elif num > 2:
-        fib = [1,1]
-        while i < (num - 1):
-            fib.append(fib[i] + fib[i-1])
-            i += 1
-    return fib
-print(fibonacci())
--- tests/test3.py
+++
@@ -1,9 +0,0 @@
-
-
-# def diagonal[matrix]
-#
-#
-# matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-# d = diagonal(matrix)
-# print(d)
-
