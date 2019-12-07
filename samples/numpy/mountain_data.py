4b5688dbf330e7b09dc7e497330c1812064d90aa
Meital Issers <meitalge@gmail.com>
2019-12-07 15:19:10 +0200

    changes

--- samples/numpy/mountain_data.py
+++ samples/numpy/mountain_data.py
@@ -1,38 +1,51 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[22]:
-
-
+import os
 import numpy as np
-from datetime import datetime
+import datetime
+
+print(os.getcwd())
+fname = os.getcwd() + "\\mountain-data.txt"
+print(fname)
 
 
-# In[23]:0
+#str2date = lambda x: np.datetime64(datetime.strptime(str(x), '%d/%m/%Y')) if x != "-" else np.datetime64('01/01/2001')
+# str2epoch = lambda x: datetime.datetime.strptime(x.decode("utf-8"), '%d/%m/%Y').timestamp() if x != b"-" else datetime.datetime.strptime('01/01/1970'.decode("utf-8"), '%d/%m/%Y').timestamp()
+str2epoch = lambda x: datetime.datetime.strptime(x.decode("utf-8"), '%d/%m/%Y').toordinal()
+epoch2str = lambda x: datetime.datetime.fromordinal(x)
+loc2float = lambda x: round((int(x[0:x.index(b"d")]) + int(x[3:x.index(b"m")])/60 + int(x[6:x.index(b"s")])/3600),3)
 
+dtype = np.dtype([("name","S20"),("height","i8"),("First_ascent_date","uint64"),("First_winter_date","uint64"),("locationN","f8"),("locationE","f8")])
 
-fname = "mountain-data.txt"
+data = np.genfromtxt(fname,dtype=dtype,skip_footer=1,skip_header=4,delimiter='\t',missing_values={2: '-',3: '-'},
+                    filling_values={2: 0,3: 0},converters = {2:str2epoch,3:str2epoch, 4: loc2float,5: loc2float} )
+print("\n******************* data *******************")
+print(data)
 
+print("\n******************* lowest 8,000 m peak *******************")
+data.sort(order=["height"])
+print(data[0])
 
-# In[75]:
+print("\n******************* The most northely, easterly, southerly and westerly peaks  *******************")
+southerly = np.amin(data["locationN"])
+northely = np.amax(data["locationN"])
+westerly = np.amin(data["locationE"])
+easterly = np.amax(data["locationE"])
+print("southerly",southerly,"northely",northely,"westerly",westerly,"easterly",easterly)
 
+print("\n******************* The most recent first ascent of the peaks *******************")
+recent_first_ascent = np.amax(data["First_ascent_date"])
+print("recent_first_ascent",epoch2str(recent_first_ascent))
 
-str2date = lambda x: np.datetime64(datetime.strptime(x, '%d/%m/%Y')) if '-' not in x else np.datetime64('2001-01-01T00:00:00')
-str2date('20/03/1977')
-str2date('-')
+print("\n******************* The first of the peaks to be climbed in winter  *******************")
+First_winter_date = data["First_winter_date"][data["First_winter_date"] > 0].min()
+print("First_winter_date",epoch2str(First_winter_date))
 
+height2feet = lambda x: float(x) * 3.2808399
+dtype2 = np.dtype([("name","S20"),("height2feet","f8"),("First_ascent_date","uint64")])
+data2 = np.genfromtxt(fname,dtype=dtype2,skip_footer=1,skip_header=4,delimiter='\t',missing_values={2: '-'},
+                    filling_values={2: 0},converters = {1:height2feet,2:str2epoch} )
+print("\n******************* data2 *******************")
+print(data2)
 
-# In[77]:
-
-
-str2date = lambda x: np.datetime64(datetime.strptime(x, '%d/%m/%Y')) if '-' not in x else np.datetime64('2001-01-01T00:00:00')
-dtype = np.dtype([("name","S20"),("height","i8"),("First ascent date","datetime64"),("First winter date","datetime64"),("locationN","S10"),("locationE","S10")])
-data = np.genfromtxt(fname,dtype=dtype,skip_footer=1,skip_header=4,delimiter='\t', converters = {2: str2date,3: str2date} )
-data
-
-
-# In[ ]:
-
-
-
-
+with open('height2feet.txt', 'w') as f:
+     data2.sort(order=["height2feet"])
+     print(data2,file=f)
--- samples/numpy/numpy_ex.py
+++ samples/numpy/numpy_ex.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
+import numpy as np
 # Create a 4X2 integer array and Prints its attributes
 # Expected Output:
 
@@ -20,25 +15,19 @@
 # 2>. Array dimensions are  2
 # 3>. Length of each element of array in bytes is  2
 
+#
+# import numpy as np
+# #a = np.arange(1,9,dtype=np.short).reshape(4,2)
+# #a = np.array([[64392,31655],[32579,0],[49248,462],[0,0]],dtype=np.int)
+# a = np.random.randint(1,100000,(4,2))
+# print(a)
+# print("shape",a.shape)
+# print("dimensions",a.ndim)
+# #print("Length of each element of array in bytes is",np.dtype(np.int).itemsize)
+# print("Length of each element of array in bytes is",a.itemsize)
 
-# In[24]:
-
-
-import numpy as np
-#a = np.arange(1,9,dtype=np.short).reshape(4,2)
-#a = np.array([[64392,31655],[32579,0],[49248,462],[0,0]],dtype=np.int)
-a = np.random.randint(1,100000,(4,2))
-print(a)
-print("shape",a.shape)
-print("dimensions",a.ndim)
-#print("Length of each element of array in bytes is",np.dtype(np.int).itemsize)
-print("Length of each element of array in bytes is",a.itemsize)
-
-
-# In[5]:
-
-
-# Create a 5X2 integer array from a range between 100 to 200 such that the difference between 
+# *************************************************************************************#
+# Create a 5X2 integer array from a range between 100 to 200 such that the difference between
 # each element is 10
 # Expected Output:
 
@@ -50,16 +39,10 @@ print("Length of each element of array in bytes is",a.itemsize)
 #  [180 190]]
 
 
-# In[31]:
+# a = np.arange(100,200,step=10).reshape(5,2)
+# a
 
-
-a = np.arange(100,200,step=10).reshape(5,2)
-a
-
-
-# In[2]:
-
-
+# *************************************************************************************
 # Following is the given numpy array return array of odd rows and even columns
 # import numpy
 
@@ -81,22 +64,16 @@ a
 #  [54 60]]
 
 
-# In[38]:
+# sampleArray = np.array([[3 ,6, 9, 12], [15 ,18, 21, 24], [27 ,30, 33, 36], [39 ,42, 45, 48], [51 ,54, 57, 60]])
+# print(sampleArray)
+#
+# print(sampleArray[0::2,1::2])
 
-
-sampleArray = np.array([[3 ,6, 9, 12], [15 ,18, 21, 24], [27 ,30, 33, 36], [39 ,42, 45, 48], [51 ,54, 57, 60]])
-print(sampleArray)
-
-print(sampleArray[0::2,1::2])
-
-
-# In[ ]:
-
-
+# *************************************************************************************
 
 # ____Split____ the array into four equal-sized sub-arrays
 # Note: Create an 8X3 integer array from a range between 10 to 34 such that the difference between each element is 1 
-#and then Split the array into four equal-sized sub-arrays.
+# and then Split the array into four equal-sized sub-arrays.
 
 # Expected Output:
 
@@ -117,18 +94,12 @@ print(sampleArray[0::2,1::2])
 # array([[22, 23, 24],[25, 26, 27]]), 
 # array([[28, 29, 30],[31, 32, 33]])]
 
-
-# In[51]:
-
-
-a = np.arange(10,34,step=1).reshape(8,3)
-print(a)
-b = a.reshape(4,2,3)
-print(b)
-
-
-# In[ ]:
-
+#
+# a = np.arange(10,34,step=1).reshape(8,3)
+# print(a)
+# b = a.reshape(4,2,3)
+# print(b)
+# *************************************************************************************
 
 # Sort following NumPy array
 # 7.1- by the second row and
@@ -153,32 +124,27 @@ print(b)
 #  [53 94 66]]
 
 
-# In[70]:
+#
+# sampleArray = np.array([[34,43,73],[82,22,12],[53,94,66]])
+# print("** sampleArray ****")
+# print(sampleArray)
+# b = np.argsort(sampleArray[1,:])
+# print("** b ****")
+# print(b)
+# c = sampleArray[:,b]
+# print("** c ****")
+# print(c)
+# print("** sampleArray ****")
+# print(sampleArray)
+# d = np.argsort(sampleArray[:,1])
+# print("** d ****")
+# print(d)
+# e = sampleArray[d,:]
+# print("** e ****")
+# print(e)
 
+# *************************************************************************************
 
-sampleArray = np.array([[34,43,73],[82,22,12],[53,94,66]])
-print("** sampleArray ****")
-print(sampleArray)
-
-b = np.argsort(sampleArray[2,:])
-print("** b ****")
-print(b)
-c = sampleArray[:,b]
-print("** c ****")
-print(c)
-print("** sampleArray ****")
-print(sampleArray)
-d = np.argsort(sampleArray[:,2])
-print("** d ****")
-print(d)
-e = sampleArray[d,:]
-print("** e ****")
-print(e)
-
-
-
-
-# In[ ]:
 
 
 # Following is the input NumPy array ____delete____ column two and ____insert____ following new column in its place.
@@ -205,43 +171,24 @@ print(e)
 #  [53 10 66]]
 
 
-# In[ ]:
+sampleArray = np.array([[34,43,73],[82,22,12],[53,94,66]])
+print(sampleArray)
+a = np.delete(sampleArray,1,axis=1)
+print(a)
+newColumn = np.array([[10,10,10]])
+b = np.insert(a,1,newColumn,axis=1)
+print(b)
 
 
 
 
 
-# In[ ]:
 
 
 
 
 
-# In[ ]:
 
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
